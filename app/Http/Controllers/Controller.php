32f79967af4ea52a33c2586361d7fb60113934b0
Darkryza <darkryza123@gmail.com>
2024-06-27 19:18:13 +0800

    prod

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+// add this to the top of the file
+use Illuminate\Support\Facades\URL;
+
+
+// add this statement to public function boot()
+if($this->app->environment('production')) {
+    URL::forceScheme('https');
+};
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
