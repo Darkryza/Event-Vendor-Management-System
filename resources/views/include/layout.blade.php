1a1b8cf94f159a3a42be8bc608951bd281ff24ec
Darkryza <darkryza123@gmail.com>
2024-06-27 18:35:07 +0800

    test

--- resources/views/include/layout.blade.php
+++ resources/views/include/layout.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
